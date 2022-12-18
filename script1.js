cf744a85568907fa072dc02242408140f2506673
Shreya-Naithani <102851285+Shreya-Naithani@users.noreply.github.com>
2022-12-18 23:35:52 +0530

    Update script1.js
    commit2

--- script1.js
+++ script1.js
@@ -19,6 +19,6 @@ function validate(event){
         error.textContent ="";
         let form = document.querySelector("form");
         form.reset();
-        window.location.href="bookStore.html";
+        window.location.href="index1.html";
     }
 }
